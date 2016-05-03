86d191f921d98e31c0780573d59f4ab1a3b12f83
Dries <dries.cronje@outlook.com>
2016-05-03 22:55:33 +0200

    Added exception handling

--- route.py
+++ route.py
@@ -5,14 +5,20 @@ with open('config.json', 'r') as f:
     config = json.load(f)
     
 def get_distance_duration(origin, destination):
-    key = config['google_key']
-    base = 'https://maps.googleapis.com/maps/api/distancematrix/json'
-    call = "{0}?&origins={1}&destinations={2}&departure_time=now&traffic_model=pessimistic&key={3}".format(base,origin.replace(' ','+'),distination.replace(' ','+'),key)
-    d = requests.get(call)
-    distance = d.json()['rows'][0]['elements'][0]['distance']['text']
-    duration = d.json()['rows'][0]['elements'][0]['duration_in_traffic']['text']
+    try:
+        key = config['google_key']
+        base = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+        call = "{0}?&origins={1}&destinations={2}&departure_time=now&traffic_model=pessimistic&key={3}".format(base,origin.replace(' ','+'),distination.replace(' ','+'),key)
+        d = requests.get(call)
+        distance = d.json()['rows'][0]['elements'][0]['distance']['text']
+        duration = d.json()['rows'][0]['elements'][0]['duration_in_traffic']['text']
+        
+        return distance,duration
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
     
-    return distance,duration
+    
     
     
     
--- weather.py
+++ weather.py
@@ -5,11 +5,16 @@ with open('config.json', 'r') as f:
     config = json.load(f)
 
 def get_current_weather():
-    key = config['weatherapi_key']
-    id = 993800
-    url = 'http://api.openweathermap.org/data/2.5/weather?id={0}&units=metric&appid={1}'.format(id,key)
-    d = requests.get(url)
-    url = 'http://openweathermap.org/img/w/{0}.png'.format(d.json()['weather'][0]['icon'])
-    temp = d.json()['main']['temp']
-    desc = d.json()['weather'][0]['description']
-    return temp, desc, url
+    try:
+        key = config['weatherapi_key']
+        id = 993800
+        url = 'http://api.openweathermap.org/data/2.5/weather?id={0}&units=metric&appid={1}'.format(id,key)
+        d = requests.get(url)
+        url = 'http://openweathermap.org/img/w/{0}.png'.format(d.json()['weather'][0]['icon'])
+        temp = d.json()['main']['temp']
+        desc = d.json()['weather'][0]['description']
+        return temp, desc, url
+    
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise

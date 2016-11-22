9eb505ef014ab6c98d5e0e9984c8fe0095897903
Chasingbob <dries.cronje@outlook.com>
2016-11-22 18:52:32 +0200

    removed traffic functionality until it is up & running again

--- route.py
+++ route.py
@@ -12,14 +12,16 @@ def get_key():
     return config.get_value('google_key')
 
 def get_current_traffic(origin,destination):
-    o = origin.replace(' ','+') 
-    d = destination.replace(' ','+')
-    key = get_key()
+    return 14, 32
+    #o = origin.replace(' ','+') 
+    #d = destination.replace(' ','+')
+    #key = get_key()
    
-    pairs = { 'origins' : o, 'destinations' : d, 'departure_time' : 'now', 'traffic_model' : 'pessimistic', 'key' : key}
-    result = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json', params = pairs)
-
-    dist = result.json()['rows'][0]['elements'][0]['distance']['text']
-    dura = result.json()['rows'][0]['elements'][0]['duration_in_traffic']['text']
-    return dist, dura
+    #pairs = { 'origins' : o, 'destinations' : d, 'departure_time' : 'now', 'traffic_model' : 'pessimistic', 'key' : key}
+    #result = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json', params = pairs)
+    #print('result')    
+    #print(result.json())
+    #dist = result.json()['rows'][0]['elements'][0]['distance']['text']
+    #dura = result.json()['rows'][0]['elements'][0]['duration_in_traffic']['text']
+    #return dist, dura
 

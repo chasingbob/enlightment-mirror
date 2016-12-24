1307b39fd78c77446afdb31b34637ca3f363d85a
Chasingbob <dries.cronje@outlook.com>
2016-12-24 09:43:55 +0200

    added off button functionality

--- static/stuff.js
+++ static/stuff.js
@@ -9,3 +9,15 @@ $(function(){
         });
     });
 });
+
+$(function(){
+    $('#btnLightOff').click(function(){
+        $.ajax({
+            url: 'http://192.168.0.112:5000/IoT/OFF',
+            type: 'GET',
+            error: function(error){
+                console.log(error);
+            }
+        });
+    });
+});

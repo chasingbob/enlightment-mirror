a33051304e152ee07d8e9404346efd7eb5c84fe0
Chasingbob <dries.cronje@outlook.com>
2016-12-24 09:34:12 +0200

    bug fix in stuff.js

--- static/stuff.js
+++ static/stuff.js
@@ -2,8 +2,7 @@ $(function(){
     $('#btnLightOn').click(function(){
     $.ajax({
         url: 'http://192.168.0.112:5000/IoT/ON',
-        type: 'GET'
-        },
+        type: 'GET',
         error: function(error){
             console.log(error);
         }
